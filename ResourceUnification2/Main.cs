4ed8c108636979d2d6d57feb83ad3cf5aa0158de
pheonixwings99 <infinitum99@gmail.com>
2026-06-18 12:12:28 -0400

    Add project files.

---
+++ ResourceUnification2/Main.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using System.Text;
+using System.Reflection;
+using UnityModManagerNet;
+using BlueprintCore.Utils;
+using Kingmaker.Blueprints.JsonSystem;
+
+namespace ResourceUnification2;
+
+public static class Main {
+    internal static Harmony HarmonyInstance;
+    internal static UnityModManager.ModEntry.ModLogger Log;
+
+    public static bool Load(UnityModManager.ModEntry modEntry) {
+        Log = modEntry.Logger;
+        modEntry.OnGUI = OnGUI;
+        HarmonyInstance = new Harmony(modEntry.Info.Id);
+        try {
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        } catch {
+            HarmonyInstance.UnpatchAll(HarmonyInstance.Id);
+            throw;
+        }
+        return true;
+    }
+
+    public static void OnGUI(UnityModManager.ModEntry modEntry) {
+
+    }
+
+    [HarmonyPatch(typeof(BlueprintsCache))]
+    public static class BlueprintsCaches_Patch {
+        private static bool Initialized = false;
+
+        [HarmonyPriority(Priority.First)]
+        [HarmonyPatch(nameof(BlueprintsCache.Init)), HarmonyPostfix]
+        public static void Init_Postfix() {
+            try {
+                if (Initialized) {
+                    Log.Log("Already initialized blueprints cache.");
+                    return;
+                }
+                Initialized = true;
+
+                Log.Log("Patching blueprints.");
+                // Insert your mod's patching methods here
+                // Example
+                // SuperAwesomeFeat.Configure()
+            } catch (Exception e) {
+                Log.Log(string.Concat("Failed to initialize.", e));
+            }
+        }
+    }
+}
